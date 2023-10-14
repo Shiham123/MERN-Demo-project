7710e8ce76b68450ed4322e7406a874afb6a3765
Shiham123 <s36651786@gmail.com>
2023-10-14 18:23:07 +0600

    upload this server

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express();
 
@@ -8,9 +9,51 @@ app.use(express.json());
 
 const port = process.env.PORT || 3000;
 
+//project1
+// YDgkp6X9ReKEMftF
+
 app.get('/', (request, response) => {
   response.send('this is backend');
 });
+
+// const uri = 'mongodb://localhost:27017';
+const uri =
+  'mongodb+srv://project1:YDgkp6X9ReKEMftF@mernstack-cluster.2mtsqtr.mongodb.net/?retryWrites=true&w=majority';
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+const run = async () => {
+  try {
+    await client.connect();
+    const database = client.db('MERNStackDB');
+    const dataCollections = database.collection('practiceDB');
+
+    app.get('/person', async (request, response) => {
+      const cursor = dataCollections.find();
+      const result = await cursor.toArray();
+      response.send(result);
+    });
+
+    app.post('/person', async (request, response) => {
+      const newData = request.body;
+      const result = await dataCollections.insertOne(newData);
+      response.send(result);
+    });
+
+    await client.db('admin').command({ ping: 1 });
+    console.log('you successfully connected to mongodb');
+  } catch (error) {
+    console.dir(error);
+  }
+};
+
+run();
 
 app.listen(port, () => {
   console.log(`server running at http://localhost:${port}`);
